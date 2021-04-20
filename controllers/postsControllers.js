d76592c7f6d0fa580963d6b33a5232cb71fcd277
Miguel Leite <amlmiguel@gmail.com>
2021-04-20 13:51:14 -0300

    fix router

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,9 +1,11 @@
 const {Post,sequelize} = require('../models');
+const { post } = require('../routes');
 
 const postsControllers = {
     index: async (req, res) => {
         let posts = await Post.findAll();
-        return res.json(posts)
+
+        return res.render('index', {listaPosts: posts});
     },
     create: async (req, res) => {
         let {texto, img, usuarios_id, n_likes} = req.body
